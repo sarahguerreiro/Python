ada73c00c061f3a80a9a518d84d4a4ebb0695164
sarahguerreiro <sdmguerre@gmail.com>
2023-04-16 17:12:38 -0300

    como fazer uma estrutura de repetição controlada, então?

--- while.py
+++ while.py
@@ -13,6 +13,14 @@ while numero_escolhido != numero_sorteado:
 print('Parabéns, vc acertou!')
 
 '''
-O while é considerada uma estrutura de repetição não controlada, uma vez q, se o user quiser digitar qualquer coisa fora do escopo da pergunta, mesm0 assim o codigo continuara rodando do mesmo jeito
+O while é considerada uma estrutura de repetição não controlada, uma vez q, se o user quiser digitar qualquer coisa fora do escopo da pergunta, mesm0 assim o codigo continuara rodando do mesmo jeito.
+Mas o while tbm pode ser uma estrutura de repetição controlada, adc ao fluxo do prog um contador q usa o laço para atualizar o valor da var e q tem um fim quando sua condição é atendida. Veja:
 
 '''
+
+contador = 0
+
+while contador < 6 :
+    print(contador)
+
+    contador = contador + 1
