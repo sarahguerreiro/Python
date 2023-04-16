1ace77332760c38b41ae3c0b81215ea78d84f28f
sarahguerreiro <sdmguerre@gmail.com>
2023-04-16 17:52:21 -0300

    adc uma var acumuladora pra fazer nossa media aritmetica

--- for.py
+++ for.py
@@ -21,8 +21,12 @@ for i in range(1, 122, 4):
     print(i)
 '''
 
+soma = 0
+
 for i in range(1,4):
     nota = float(input(f'informe a sua nota: {i}: '))
+
+    soma = soma + nota
 
 
 '''
@@ -31,4 +35,6 @@ INJETANDO UMA VAR DENTRO DE UMA STRING:
  é ela q vc vai injetar na string do input, 
  adc um f na frente da string em questão,logo antes d as aspas, o q vai indicar q será feita uma injeção naquela string, depois vc so precisa abrir chaves na posição q vc quer q a var esteja na string e coloca a var dentro
 '''
+#VARIÁVEL ACUMULADORA: é quando vc cria uma var com o 0 por ser um numero neutro e adc essa var no bloco de codico reatribuindo o valor dela a soma dela mesma + valor recebido do user 
+
 
