44e24d9a9250763174a6348277b19aa542e2ce5c
sarahguerreiro <sdmguerre@gmail.com>
2023-04-16 17:39:02 -0300

    fazendo um laço for pra coletar mais de 1 valor do user automaticamente e injetando uma var numa string

--- for.py
+++ for.py
@@ -1,8 +1,8 @@
 
 '''
-range com valor de inicio:
+range vai de 0 ate o numero setado -1:
 
-for i in range(5):
+for i in range(6):
     print(i)
 '''
 
@@ -10,7 +10,7 @@ for i in range(5):
 '''
 range com valor de inicio e valor de parada:
 
-for i in range(1, 9):
+for i in range(1, 11 ):
     print(i)
 '''
 
@@ -22,5 +22,13 @@ for i in range(1, 122, 4):
 '''
 
 for i in range(1,4):
-    nota = float(input(f'informe a sua nota: [i]'))
+    nota = float(input(f'informe a sua nota: {i}: '))
+
+
+'''
+INJETANDO UMA VAR DENTRO DE UMA STRING: 
+ Como a var i é o q vai mudar a cada iteração, 
+ é ela q vc vai injetar na string do input, 
+ adc um f na frente da string em questão,logo antes d as aspas, o q vai indicar q será feita uma injeção naquela string, depois vc so precisa abrir chaves na posição q vc quer q a var esteja na string e coloca a var dentro
+'''
 
