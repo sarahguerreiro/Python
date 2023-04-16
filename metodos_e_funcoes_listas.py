928027a9720f94894b47bbac4b88affca18c4a85
sarahguerreiro <sdmguerre@gmail.com>
2023-04-16 18:56:09 -0300

    metodos e funções em listas: append- metodo para adc itens no final de uma lista

---
+++ metodos_e_funcoes_listas.py
@@ -0,0 +1,16 @@
+# METODOS DE LISTAS: É como se fosse uma função atrelada a uma var
+
+lista = [1, 3, 12, 8, 2]
+
+# APPEND - Metodo usado quando vc precisa adcionar um elemento ao final de uma lista
+
+print('antes do append', lista)
+lista.append(3)
+print('depois do append', lista)
+
+# insert
+
+
+# extend
+
+
