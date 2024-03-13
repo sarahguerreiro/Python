b78daa2c0a018cc36efdda84af5506302f06e0b2
Sarah <sdmguerre@gmail.com>
2024-03-12 22:15:15 -0300

    conteudo da semana 2 univesp

---
+++ univesp/semana2/maisOperadores.py
@@ -0,0 +1,22 @@
+# + - * 3 / 2 = 1.5
+
+# // divisão inteira. O valor retornado é apenas o quociente da divisão, sem o resto. 3//2 = 1
+
+# % módulo: tras o resto da divisão
+
+# ** potencia: 2**2 = 4
+
+print(3+7)
+print(3*2)
+print(5/2)
+print(5//2)
+print(5%2)
+print(4/2)
+print(4//2)
+print(4%2)
+print(2*3+1)
+print((3+1)*3)
+print(4.321/3+10)
+print(4.321 / (3+10))
+print(4.321 // (3+10))
+print(4.321 // 3+10)
