d2a18a0871434712932bd3576cff00fd69d942eb
Sarah <sdmguerre@gmail.com>
2024-03-13 14:45:57 -0300

    variaveis - semna 2

---
+++ univesp/semana2/variaveis.py
@@ -0,0 +1,58 @@
+# OPERADORES ARITMETICOS TEM PRECEDENCIA SOBRE OPERADOR DE ATRIBUIÇÃO
+n = 3+3
+x = n - 2 
+print(n)
+print(2*n)
+print(x)
+print(n)
+
+
+"""
+--------- VARIAVEIS EM PY ------------
+
+
+► NOME DE VARIAVEL:
+
+ → nome em minusculo é uma pratica comum e recomendada comecar os nomes de variaveis em minuscula
+    nomevarminusc
+
+ → NOME EM MAIUSCULO GERALMENTE SAO USADAS P CONSTANTES/VARIAVEIS GLOBAIS:
+    NOMEVARMAIUSC
+
+ → NOME COM USO DE UNDESCORE_ snake_case é muito utilizada em nomes de variaveis em python
+    nome_com_underline
+
+ → EXCETO PARA O 1o CARACTERE, PODEM OS DIGITOS DE 0 A 9
+    nomeCom1Numero
+
+ CONVENÇÃO PRA DEFINIR BONS NOMES: 
+ → USAR NOMES SIGNIFICATIVOS
+   preco AO INVES DE p
+→ NOMES COM MAIS DE UMA PALAVRA: 
+   Separar por_ ou usar camelCase
+→ NOMES CURTOS
+   se muito significativos sao melhores do q nomes longos
+
+
+
+"""
+
+nome = 'Sarah'
+idade = 27
+sexo = 'F'
+estado_civil = 'solteira'
+
+print(nome)
+print(idade)
+print(sexo)
+print(estado_civil)
+
+grausCelsius = 31
+grausk = grausCelsius + 273
+grausF = 1.8 * grausCelsius + 32
+
+print(grausCelsius)
+
+print(grausF)
+
+print(grausk)
